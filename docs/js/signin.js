7bb6ccc7c2ba38d602942d34415d216c22cc7e37
Evelina Wongosari <ewongosari@users.noreply.github.com>
2018-05-03 15:16:47 -0400

    first push

---
+++ docs/js/signin.js
@@ -0,0 +1,28 @@
+var provider = new firebase.auth.GoogleAuthProvider();
+function signIn() {
+  firebase.auth().signInWithPopup(provider).then(function(result) {
+    // This gives you a Google Access Token. You can use it to access the Google API.
+    var token = result.credential.accessToken;
+    // The signed-in user info.
+    var user = result.user;
+    console.log(user + "signed in!");
+    alert("Welcome" + user + "!");
+  }).catch(function(error) {
+  // Handle Errors here.
+    var errorCode = error.code;
+    var errorMessage = error.message;
+    // The email of the user's account used.
+    var email = error.email;
+    // The firebase.auth.AuthCredential type that was used.
+    var credential = error.credential;
+  });
+}
+
+function signOut() {
+  firebase.auth().signOut().then(function() {
+    console.log(user + "signed out!");
+    alert("See you later" + user + "!");
+  }).catch(function(error) {
+    console.log("error with sign out");
+  });
+}
